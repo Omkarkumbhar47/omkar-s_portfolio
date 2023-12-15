2db765188cbce02418acc1165890b37d29bf59b6
Omkarkumbhar47 <142390414+Omkarkumbhar47@users.noreply.github.com>
2023-12-15 15:15:42 +0530

    added the index and all files

---
+++ main.js
@@ -0,0 +1,9 @@
+<script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+      <script id="auto_ty">
+        var typed = new Typed(".auto-input", {
+          strings: ["Omkar's  Portfolio"],
+          typeSpeed: 200,
+          backspeed: 700,
+          loop: true,
+        });
+      </script>

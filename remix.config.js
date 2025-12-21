b369640f3b48b906ba338f9d02cd6f14505076f6
Omkarkumbhar47 <kumbharomkar747@gmail.com>
2025-12-21 23:51:24 +0530

    uninstall cloudflare and added the remix.config.js file

---
+++ remix.config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
+module.exports = {
+  serverBuildTarget: "node-cjs",
+  ignoredRouteFiles: ["**/.*"],
+  appDirectory: "app",
+  // adapter specific config (optional)
+  // You can add more config here if needed
+};
